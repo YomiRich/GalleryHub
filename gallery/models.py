856fefe5a60c3a277161d7b43b6fe41fca283609
Yomi Rich <ymueni@gmail.com>
2019-10-07 13:23:42 +0300

    Add image model and pip installed pillow to the image field.

--- gallery/models.py
+++ gallery/models.py
@@ -6,3 +6,12 @@ class Category (models.Model):
     
 class Location (models.Model):
     location = models.CharField(max_length =30)
+    
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/',default='DEFAULT VALUE')
+    image_name =models.CharField(max_length =60) 
+    image_description = models.CharField(max_length=255)
+    image_location = models.ForeignKey('Location',on_delete=models.CASCADE,)
+    image_category = models.ManyToManyField(Category)
+    
+    
