6b9e46dba90994607a2c63f8fbdd95d41e328798
Yomi Rich <ymueni@gmail.com>
2019-10-07 14:46:51 +0300

    Run tests severally and all passed

--- gallery/models.py
+++ gallery/models.py
@@ -3,10 +3,24 @@ from django.db import models
 # Create your models here.
 class Category (models.Model):
     category = models.CharField(max_length =30)
+    def __str__(self):
+        return self.category
     
+    def save_category(self):
+        self.save()
+        
 class Location (models.Model):
     location = models.CharField(max_length =30)
     
+    def __str__(self):
+        return self.location
+    
+    class Meta:
+        ordering = ['location']
+        
+    def save_location(self):
+        self.save()
+        
 class Image(models.Model):
     image = models.ImageField(upload_to='images/',default='DEFAULT VALUE')
     image_name =models.CharField(max_length =60) 
@@ -14,4 +28,10 @@ class Image(models.Model):
     image_location = models.ForeignKey('Location',on_delete=models.CASCADE,)
     image_category = models.ManyToManyField(Category)
     
+    def __str__(self):
+        return self.image_name
     
+    def save_image(self):
+        self.save()
+    
+
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
+from .models import Category,Location,Image
 
 # Create your tests here.
+
+class CategoryTestClass(TestCase):
+    
+    # Set up method
+    def setUp(self):
+        self.Fashion= Category(category='Fashion')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Fashion,Category))
+        
+    def tearDown(self):
+        Category.objects.all().delete()
+        
+    def test_save_method(self):
+        self.Fashion.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories)>0)
+    
+class LocationTestClass(TestCase):
+    
+    def setUp(self):
+        self.Santorini=Location(location='Santorini')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Santorini,Location))
+        
+    def tearDown(self):
+        Location.objects.all().delete()
+        
+    def test_save_method(self):
+        self.Santorini.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)>0)
+        
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.test_category = Category(category=list('Fashion'))
+        self.test_category.save_category()
+
+        self.location = Location(location="Santorini")
+        self.location.save_location()
+
+        self.image = Image(id=1,image_name="Greece",image_description="Number one tourist destination",category=self.test_category,location=self.location,)
+        self.image.save_image()
+
+    def tearDown(self):
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+        Image.objects.all().delete()       
