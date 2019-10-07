3f1eb9dcdeb93b4722db6acb458f56f5df5ccb68
Yomi Rich <ymueni@gmail.com>
2019-10-07 13:09:36 +0300

    Add Location model. run check successful

--- gallery/models.py
+++ gallery/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Category (models.Model):
     category = models.CharField(max_length =30)
     
+class Location (models.Model):
+    location = models.CharField(max_length =30)
