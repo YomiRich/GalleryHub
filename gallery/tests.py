ebe89de8c1c1f019b5113de9a08a8fab1f086bde
Yomi Rich <ymueni@gmail.com>
2019-10-11 11:53:12 +0300

    Add tests, search feature and modal to view image in an expanded format

--- gallery/tests.py
+++ gallery/tests.py
@@ -19,6 +19,34 @@ class CategoryTestClass(TestCase):
         self.Fashion.save_category()
         categories = Category.objects.all()
         self.assertTrue(len(categories)>0)
+        
+    #Testing save method
+    def test_save_method(self):
+        self.Fashion.save_category()
+        categories=Category.objects.all()
+        self.assertTrue(len(categories)>0)  
+
+    #Testing save multiple
+    def test_save_multiple_categories(self):
+        self.Fashion.save_category()
+
+        test_Fashion=Category(category_name='Fashion')
+        test_Fashion.save_category()
+
+        categories=Category.objects.all()
+        self.assertEqual(len(categories),2)
+
+
+    #Testiing delete method
+    def test_delete_method(self):
+        self.Fashion.save_category()
+
+        test_Fashion=Category(category_name='Fashion')
+        test_Fashion.save_category()
+
+        test_Fashio.delete_category()
+        categories=Category.objects.all()
+        self.assertEqual(len(categories),1)   
     
 class LocationTestClass(TestCase):
     
@@ -35,8 +63,30 @@ class LocationTestClass(TestCase):
         self.Santorini.save_location()
         locations = Location.objects.all()
         self.assertTrue(len(locations)>0)
+    
+    def test_delete_method(self):
+        self.Santorini.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)>0)   
+        test_Santorini=Location(location_name='Santorini')
+        test_Santorini.save_location()
+
+        test_Santorini.delete_location()
+        locations=Location.objects.all()
+        self.assertEqual(len(location),1)
+        
+    #Testing save multiple
+    def test_save_multiple_locations(self):
+        self.Santorini.save_locations()
+
+        test_Santorini=Location(location_name='Santorini')
+        test_Santorini.save_locations()
+
+        locations=Location.objects.all()
+        self.assertEqual(len(locations,2)
         
 class ImageTestClass(TestCase):
+    
     def setUp(self):
         self.test_category = Category(category=list('Fashion'))
         self.test_category.save_category()
@@ -50,4 +100,11 @@ class ImageTestClass(TestCase):
     def tearDown(self):
         Category.objects.all().delete()
         Location.objects.all().delete()
-        Image.objects.all().delete()       
+        Image.objects.all().delete()    
+        
+    def test_get_photos(self):
+        photos=Image.get_photos()
+        self.assertTrue(len(photos)>0)
+
+    
+        
